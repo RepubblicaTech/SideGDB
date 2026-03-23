9b30661dcfa48c87c84eae28bc694468a55ac61d
Omar <omarino@elshorbagy.it>
2026-03-23 16:33:28 +0100

    Remove unused method
    i'll likely rewrite it later

--- backend/model.py
+++ backend/model.py
@@ -38,25 +38,3 @@ class SideModel:
 
     def read(self, attempts):
         return self.__gdbMI.readResponse(attempts)
-
-    def getThreadInfo(self):
-        responses = self.__gdbMI.threadInfo()
-        """
-        threadsResponse = self.selectResponse(responses, ("token", self.token()))
-
-        threadsFrame: dict = {
-            "current_thread": threadsResponse["payload"]["current-thread-id"],
-            "threads": []
-        }
-
-        for thread in threadsResponse["payload"]["threads"]:
-            threadsFrame["threads"].append({
-                "id": thread["id"],
-                "state": thread["state"],
-                "frame": thread["frame"]
-            })
-
-        self.getThreadInfo = threadsFrame["current_thread"]
-
-        return threadsFrame
-        """
