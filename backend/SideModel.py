29d76b3239dffdd40c1b09d5d5b44d75c68adb96
Omar <omarino@elshorbagy.it>
2026-03-25 18:32:19 +0100

    renamed model to SideModel

---
+++ backend/SideModel.py
@@ -0,0 +1,43 @@
+from typing import Any, List
+
+from backend import GDBMI
+
+class SideModel:
+    currentThreadId: int
+    currentBreakpoint: str
+
+    registers: dict
+    breakpoints: list[dict]
+
+    variables: list[dict]
+
+    def __init__(self, gdbMI: GDBMI.GdbMI) -> None:
+        self.__gdbMI = gdbMI
+
+    def send(self, cmd):
+        return self.__gdbMI.sendCmd(cmd)
+
+    def read(self, attempts):
+        return self.__gdbMI.readResponse(attempts)
+
+    def terminate(self):
+        self.__gdbMI.exit()
+
+    def selectResponse(self, gdbMIResponse: dict | List[dict], *keys: tuple[str, Any]) -> dict:
+        keysCount = len(keys)
+        finds = 0
+
+        if (type(gdbMIResponse) is list):
+            for r in gdbMIResponse:
+                selected = self.selectResponse(r, *keys)
+                if (selected != {}):
+                    return selected
+        elif (type(gdbMIResponse) is dict):
+            for key in keys:
+                if (gdbMIResponse.get(key[0]) == key[1]):
+                    finds += 1
+
+            if (finds == keysCount):
+                return gdbMIResponse
+
+        return {}
