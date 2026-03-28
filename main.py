799978371d9d7424e828acce2915d2a6d11d0798
Omar <omarino@elshorbagy.it>
2026-03-28 12:34:26 +0100

    Set up python `logging`
    For libs like pygdbmi

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import logging
 from loguru import logger
-from PySide6.QtWidgets import QApplication
 
+from PySide6.QtWidgets import QApplication
 from ui.DebuggerUI import DebuggerUI
 
 APPLICATION_TITLE = "SideGDB"
 
 if __name__ == "__main__":
     logger.info("Reset.")
+    logging.basicConfig(level=logging.DEBUG)
 
     app = QApplication([])
     window = DebuggerUI(APPLICATION_TITLE)
