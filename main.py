25f1dfdd45077912f0a448adf3c30ce16ea0f877
Omar <omarino@elshorbagy.it>
2026-03-29 13:52:48 +0200

    Add window icon
    It's not working for me smh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+from PySide6.QtGui import QIcon
 from loguru import logger
 
 from PySide6.QtWidgets import QApplication
@@ -11,6 +12,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
     app = QApplication([])
+    windowIcon = QIcon("assets/sidegdb_logo.png")
+    app.setWindowIcon(windowIcon)
     window = DebuggerUI(APPLICATION_TITLE)
     window.show()
     window.statusBar().showMessage("Ready.")
