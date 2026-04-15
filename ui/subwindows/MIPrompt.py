c3d6500b11437d4508482e4eaa2696381cafd00e
Omar <omarino@elshorbagy.it>
2026-04-15 15:14:52 +0200

    FIX: Remove all interaction with MI log widget
    If you were to click the cursor in the widget, it would set the cursor position there and next time you'd output something it would start from that position, Qt skill issue

--- ui/subwindows/MIPrompt.py
+++ ui/subwindows/MIPrompt.py
@@ -2,13 +2,14 @@ from pprint import pformat
 from typing import List
 
 from PySide6.QtCore import QTimer
-from PySide6.QtGui import QPalette
+from PySide6.QtGui import QPalette, Qt
 
 from backend.SideModel import SideModel
 from PySide6.QtWidgets import QLineEdit, QTextBrowser, QVBoxLayout, QWidget
 
 class MIPrompt(QWidget):
     def __init__(self, model: SideModel):
+        self.canAutoscroll = True
         super().__init__()
 
         layout = QVBoxLayout()
@@ -16,22 +17,20 @@ class MIPrompt(QWidget):
         self.miPrompt = QLineEdit(placeholderText="Type any GDBMI command in here (-thread-info)")
         self.miOutput = QTextBrowser(self)
         self.miOutput.setPlaceholderText("Responses get printed here")
+        self.miOutput.setReadOnly(True)
+        self.miOutput.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
 
         layout.addWidget(self.miPrompt)
         layout.addWidget(self.miOutput)
-
 
         self.setLayout(layout)
 
         self.miPrompt.returnPressed.connect(self.sendCommand)
 
         MIPromptManager.setOutputView(self.miOutput)
-
         self.model = model
         read = self.model.read(-1)
         MIPromptManager.printFormatted(list(read))
-
-        self.canAutoscroll = True
 
     def reset(self):
         self.miPrompt.setText("")
