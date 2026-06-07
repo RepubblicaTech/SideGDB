d613b362ae38f7d3656480b94b54c73d4028d866
Omar <omarino@elshorbagy.it>
2026-06-07 11:08:44 +0200

    feat(application): check if pre-release is running

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import json
 import logging
 from PySide6.QtGui import QIcon
 from loguru import logger
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
 from ui.DebuggerUI import DebuggerUI
 
 APPLICATION_TITLE = "SideGDB"
+ABOUTJSON_PATH = "assets/about.json"
 
 if __name__ == "__main__":
     logger.info("Reset.")
@@ -15,6 +17,16 @@ if __name__ == "__main__":
     windowIcon = QIcon("assets/sidegdb_logo.png")
     app.setWindowIcon(windowIcon)
     window = DebuggerUI(APPLICATION_TITLE)
+
+    with open(ABOUTJSON_PATH, "r") as f:
+        obj = json.load(f)
+        if (obj.get("prerelease", None)):
+            QMessageBox(QMessageBox.Icon.Warning,
+                        "SideGDB development release",
+                        "This is alpha/prerelease software.\n"
+                        "It might be subject to unknown bugs or straight up crashes/hangups.\n"
+                        "Do NOT report these issues as the developer might be fixing them.").exec()
+
     window.show()
     window.statusBar().showMessage("Ready.")
 
