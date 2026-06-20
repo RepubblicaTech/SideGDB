5a2b922476f572a9b2e785e9512d2c6cb7526b2e
Omar <omarino@elshorbagy.it>
2026-06-20 13:53:53 +0200

    Unused imports #8
    yes i counted them

--- ui/subwindows/BreakManager.py
+++ ui/subwindows/BreakManager.py
@@ -1,5 +1,4 @@
-from PySide6.QtGui import QStandardItem
-from PySide6.QtWidgets import QDialog, QGridLayout, QLineEdit, QMessageBox, QPushButton, QTableView, QWidget
+from PySide6.QtWidgets import QDialog, QGridLayout, QLineEdit, QPushButton, QTableView, QWidget
 from loguru import logger
 
 from backend.SideModel import SideModel
