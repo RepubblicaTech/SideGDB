84b6b74ee14fb822266947a74213ca30c8eab50e
Omar <omarino@elshorbagy.it>
2026-06-29 18:44:34 +0200

    feat(app, aboutbox): use QResource paths for assets

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from PySide6.QtCore import QFile, QIODeviceBase, QTextStream
 from PySide6.QtGui import QIcon
 from loguru import logger
 
@@ -7,7 +8,7 @@ from PySide6.QtWidgets import QApplication, QMessageBox
 from ui.DebuggerUI import DebuggerUI
 
 APPLICATION_TITLE = "SideGDB"
-ABOUTJSON_PATH = "assets/about.json"
+ABOUTJSON_PATH = ":/app-assets/about"
 
 if __name__ == "__main__":
     logger.info("Reset.")
@@ -19,26 +20,32 @@ if __name__ == "__main__":
     app.setApplicationName(APPLICATION_TITLE)
     window = DebuggerUI(APPLICATION_TITLE)
 
-    with open(ABOUTJSON_PATH, "r") as f:
-        obj = json.load(f)
-        semVersion = f"{obj["major"]}.{obj["minor"]}.{obj["patch"]}"
-        if (obj.get("prerelease", None)):
-            semVersion += f"-{obj["prerelease"]["version"]}.{obj["prerelease"]["id"]}"
-        app.setApplicationVersion(semVersion)
+    aboutJson = QFile(ABOUTJSON_PATH)
+    if (not aboutJson.open(QFile.OpenModeFlag.ReadOnly | QIODeviceBase.OpenModeFlag.Text)):
+        logger.error("Missing about.json. Quitting.")
+        window.close()
+        app.exit(-1)
 
-        if (obj.get("prerelease", None)):
-            match (obj["prerelease"]["version"]):
-                case "alpha":
-                    QMessageBox(QMessageBox.Icon.Warning,
-                                "SideGDB development release",
-                                "This is alpha software.\n"
-                                "Do NOT report bugs to the developer as he might be fixing them right now...").exec()
-                case _:
-                    QMessageBox(QMessageBox.Icon.Warning,
-                                "SideGDB development release",
-                                "This is beta/prerelease software.\n"
-                                "Make sure to report any bugs on the repo's Issues tab.\n"
-                                "https://github.com/RepubblicaTech/SideGDB/issues").exec()
+    f = QTextStream(aboutJson)
+    obj = json.loads(f.readAll())
+
+    semVersion = f"{obj["major"]}.{obj["minor"]}.{obj["patch"]}"
+    if (obj.get("prerelease", None)):
+        semVersion += f"-{obj["prerelease"]["version"]}.{obj["prerelease"]["id"]}"
+        match (obj["prerelease"]["version"]):
+            case "alpha":
+                QMessageBox(QMessageBox.Icon.Warning,
+                            "SideGDB development release",
+                            "This is alpha software.\n"
+                            "Do NOT report bugs to the developer as he might be fixing them right now...").exec()
+            case _:
+                QMessageBox(QMessageBox.Icon.Warning,
+                            "SideGDB development release",
+                            "This is beta/prerelease software.\n"
+                            "Make sure to report any bugs on the repo's Issues tab.\n"
+                            "https://github.com/RepubblicaTech/SideGDB/issues").exec()
+
+    app.setApplicationVersion(semVersion)
 
     window.show()
     window.statusBar().showMessage("Ready.")
--- src/ui/subwindows/AboutBox.py
+++ src/ui/subwindows/AboutBox.py
@@ -1,9 +1,10 @@
 import json
-from PySide6.QtCore import Qt
+from venv import logger
+from PySide6.QtCore import QFile, QIODeviceBase, QTextStream, Qt
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QDialog, QLabel, QPushButton, QVBoxLayout, QWidget, QApplication
 
-ABOUTJSON_PATH = "assets/about.json"
+ABOUTJSON_PATH = ":/app-assets/about"
 
 class AboutBox(QDialog):
     def __init__(self, parent: QWidget, appTitle: str):
@@ -16,13 +17,15 @@ class AboutBox(QDialog):
         self.setLayout(layout)
 
         banner = QLabel()
-        banner.setPixmap(QPixmap("assets/sidegdb_banner.png").scaledToWidth(self.width() - 10, Qt.TransformationMode.SmoothTransformation))
+        banner.setPixmap(QPixmap(":/app-assets/banner").scaledToWidth(self.width() - 10, Qt.TransformationMode.SmoothTransformation))
 
-        # json thingies
-        codeName = ""
-        with open(ABOUTJSON_PATH, "r") as f:
-            obj = json.load(f)
-            codeName = obj["codename"] or ""
+        aboutJson = QFile(ABOUTJSON_PATH)
+        if (not aboutJson.open(QFile.OpenModeFlag.ReadOnly | QIODeviceBase.OpenModeFlag.Text)):
+            logger.error("Missing about.json. Quitting.")
+            return
+        f = QTextStream(aboutJson)
+        obj = json.loads(f.readAll())
+        codeName = obj["codename"] or ""
 
         aboutLabel = QLabel(f"{self.appTitle} {codeName}\nVersion {QApplication.instance().applicationVersion()}")
         creditsLabel = QLabel("Some icons by <a href=\"http://p.yusukekamiyamane.com/\">Yusuke Kamiyamane</a>")
